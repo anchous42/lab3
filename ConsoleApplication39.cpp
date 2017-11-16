8ac951f456846c1ee15cfd02ba6c808c565172dd
anchous42 <anna_bolshak@mail.ru>
2017-11-16 04:28:06 +0300

    классы

---
+++ ConsoleApplication39.cpp
@@ -0,0 +1,159 @@
+// ConsoleApplication39.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class Home
+{
+public:
+	void set() {
+
+
+	}
+	void show() {
+
+
+
+	}
+protected:
+
+public:
+	void OpenForWriting(); // открыть для записи
+	void OpenForReading(bool bFromBeginning); // открыть для чтения
+	void Close(); // закрыт
+};
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+class main
+{
+public:
+	void set() {
+
+
+	}
+	void show() {
+
+
+
+	}
+protected:
+
+public:
+	void OpenForWriting(); // открыть для записи
+	void OpenForReading(bool bFromBeginning); // открыть для чтения
+	void Close(); // закрыт
+};
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+class purchased 
+{
+public:
+	void set() {
+
+
+	}
+	void show() {
+
+
+
+	}
+protected:
+
+public:
+	void OpenForWriting(); // открыть для записи
+	void OpenForReading(bool bFromBeginning); // открыть для чтения
+	void Close(); // закрыт
+};
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+class payment
+{
+public:
+	void set() {
+
+
+	}
+	void show() {
+
+
+
+	}
+protected:
+
+public:
+	void OpenForWriting(); // открыть для записи
+	void OpenForReading(bool bFromBeginning); // открыть для чтения
+	void Close(); // закрыт
+};
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+class price
+{
+public:
+	void set() {
+
+
+	}
+	void show() {
+
+
+
+	}
+protected:
+
+public:
+	void OpenForWriting(); // открыть для записи
+	void OpenForReading(bool bFromBeginning); // открыть для чтения
+	void Close(); // закрыт
+};
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+class assortment
+{
+public:
+	void set() {
+
+
+	}
+	void show() {
+
+
+
+	}
+protected:
+
+public:
+	void OpenForWriting(); // открыть для записи
+	void OpenForReading(bool bFromBeginning); // открыть для чтения
+	void Close(); // закрыт
+};
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
